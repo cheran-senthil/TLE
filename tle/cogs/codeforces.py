d130cd7d38e3caf10870a1aea2e90dadf64689d8
meooow25 <soumiksarkar.3120@gmail.com>
2019-03-31 21:38:36 +0530

    Add totals to solved command plot. Fix underscore label issue. Improved error handling.

--- tle/cogs/codeforces.py
+++ tle/cogs/codeforces.py
@@ -21,11 +21,8 @@ class Codeforces(commands.Cog):
     @commands.command(brief='Show histogram of solved problems on CF.')
     async def solved(self, ctx, *handles: str):
         """Shows a histogram of problems solved on Codeforces for the handles provided."""
-        if not handles:
-            await ctx.send('Specify some handles')
-            return
-        if len(handles) > 5:
-            await ctx.send('No more than 5 handles at once are supported')
+        if not handles or len(handles) > 5:
+            await ctx.send('Number of handles must be between 1 and 5')
             return
 
         url = API_BASE_URL + '/user.status'
@@ -33,15 +30,20 @@ class Codeforces(commands.Cog):
 
         for handle in handles:
             params = {'handle': handle}
-            async with self.session.get(url, params=params) as resp:
-                respjson = await resp.json()
+            try:
+                async with self.session.get(url, params=params) as resp:
+                    respjson = await resp.json()
+            except aiohttp.ClientConnectionError as e:
+                logging.error(f'Request to CF API encountered error: {e}')
+                await ctx.send('Error connecting to Codeforces API')
+                return
+
             if respjson['status'] == 'FAILED':
                 if 'not found' in respjson['comment']:
-                    await ctx.send(f'Invalid handle: *{handle}*')
+                    await ctx.send(f'Handle not found: `{handle}`')
                 else:
-                    logging.info(f'Request to CF API failed with status {respjson["status"]} '
-                                 f'and comment {respjson["comment"]}')
-                    await ctx.send('Codeforces API error :(')
+                    logging.info(f'CF API denied request with comment {respjson["comment"]}')
+                    await ctx.send('Codeforces API denied the request, please make sure handles are valid.')
                 return
 
             submissions = respjson['result']
@@ -49,15 +51,13 @@ class Codeforces(commands.Cog):
             for submission in submissions:
                 if submission['verdict'] == 'OK':
                     problem = submission['problem']
-                    # CF problems don't have IDs! Just hope names don't clash?
+                    # CF problems don't have IDs! Just hope (name, rating) pairs don't clash?
                     name = problem['name']
                     rating = problem.get('rating')
                     if rating:
                         problems.add((name, rating))
 
             ratings = [rating for name, rating in problems]
-            from collections import Counter
-            logging.debug(f'Problems: {handle}, {sorted(Counter(ratings).items())}')
             allratings.append(ratings)
 
         # Adjust bin size so it looks nice
@@ -66,10 +66,12 @@ class Codeforces(commands.Cog):
 
         # matplotlib ignores labels that begin with _
         # https://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.legend
-        adjusted_handles = [handle.lstrip('_') for handle in handles]
+        # Add zero-width space to work around this
+        zero_width_space = '\u200b'
+        labels = [f'{zero_width_space}{handle}: {len(ratings)}' for handle, ratings in zip(handles, allratings)]
 
         plt.clf()
-        plt.hist(allratings, bins=histbins, label=adjusted_handles)
+        plt.hist(allratings, bins=histbins, label=labels)
         plt.title('Histogram of problems solved on Codeforces')
         plt.xlabel('Problem rating')
         plt.ylabel('Number solved')
