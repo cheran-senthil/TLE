423e200ea0b27d261c19808f2955dae9d79065ac
Narut <narut.se@gmail.com>
2019-04-01 18:46:18 -0500

    added sethandle, gethandle, showhandles, and a handle helper class

---
+++ handle_conn/handle_conn.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+class HandleConn():
+    def __init__(self, dbfile):
+        self.conn = sqlite3.connect(dbfile)
+        self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS user_handle(
+                id TEXT PRIMARY KEY,
+                handle TEXT 
+            )
+        ''')
+    
+    def sethandle(self, id, handle):
+        """ returns 1 if set, 0 if not """
+        query = '''
+            INSERT OR REPLACE INTO user_handle (id, handle) values
+            (?, ?)
+        '''
+        cur = self.conn.cursor()
+        cur.execute(query, (id, handle))
+        self.conn.commit()
+        return cur.rowcount
+    
+    def gethandle(self, id):
+        """ returns string or None """
+        query = 'SELECT handle FROM user_handle WHERE id = ?'
+        cur = self.conn.cursor()
+        cur.execute(query, (id,))
+        res = cur.fetchone()
+        if res: return res[0]
+        return None
+
+    def getallhandles(self):
+        """ returns list of (id, handle) """
+        query = 'SELECT id, handle FROM user_handle'
+        cur = self.conn.cursor()
+        cur.execute(query)
+        return cur.fetchall()
+
---
+++ tle/cogs/handles.py
@@ -0,0 +1,68 @@
+import discord
+from discord.ext import commands
+from handle_conn.handle_conn import HandleConn
+from tabulate import tabulate
+
+class Handles(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.url = 'https://codeforces.com/profile/{}'
+        self.conn = HandleConn('handles.db')
+
+    @commands.command(brief='sethandle')
+    @commands.has_role('Admin')
+    async def sethandle(self, ctx, member: discord.Member, handle: str):
+        """set handle"""
+        if not handle:
+            await ctx.send('syntax: sethandle [name] [handle]')
+            return
+        if not member:
+            await ctx.send('Member not found!')
+            return
+        msg = None
+        try:
+            r = self.conn.sethandle(member.id, handle)
+            if r == 1:
+                url = self.url.format(handle)
+                msg = 'sethandle: {} set to {}'.format(member.name, url)
+            else:
+                msg = 'No rows affected'
+        except:
+            msg = 'sethandle error!'
+        await ctx.send(msg)
+
+    @commands.command(brief='gethandle')
+    async def gethandle(self, ctx, member: discord.Member):
+        """get handle"""
+        if not member:
+            await ctx.send('Member not found!')
+        msg = None
+        try:
+            res = self.conn.gethandle(member.id)
+            if res:
+                url = self.url.format(res)
+                msg = 'gethandle: {} at {}'.format(member.name, url)
+            else:
+                msg = 'gethandle: {} not found'.format(member.name)
+        except:
+            msg = 'gethandle error!'
+        await ctx.send(msg)
+
+    @commands.command(brief="show all handles")
+    async def showhandles(self, ctx):
+        """ show all handles """
+        msg = None
+        try:
+            converter = commands.MemberConverter()
+            res = self.conn.getallhandles()
+            table = []
+            for id, handle in res:
+                name = await converter.convert(ctx, id)
+                table.append((name, handle))
+            msg = '```\n{}\n```'.format(tabulate(table, headers=('name', 'handle')))
+        except Exception  as e:
+            msg = 'showhandles error! {}'.format(e)
+        await ctx.send(msg)
+
+def setup(bot):
+    bot.add_cog(Handles(bot))
