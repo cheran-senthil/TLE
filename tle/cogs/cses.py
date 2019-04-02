4704ed02d7d81e0e2093c3d60f13e80678852b0c
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-03 01:12:31 +0530

    Add Whitespace

--- tle/cogs/cses.py
+++ tle/cogs/cses.py
@@ -1,14 +1,13 @@
-from collections import defaultdict
-
 import asyncio
+from collections import defaultdict
 
 import discord
 from discord.ext import commands
-
 from tle.cogs.util import cses_scraper as cses
 
+
 def score(placings):
-    points = {1:8, 2:5, 3:3, 4:2, 5:1}
+    points = {1: 8, 2: 5, 3: 3, 4: 2, 5: 1}
     #points = {1:5, 2:4, 3:3, 4:2, 5:1}
     return sum(points[rank] for rank in placings)
 
@@ -34,33 +33,31 @@ class CSES(commands.Cog):
 
         try:
             for pid in await cses.get_problems():
-                fast,short = await cses.get_problem_leaderboard(pid)
+                fast, short = await cses.get_problem_leaderboard(pid)
                 for i in range(len(fast)):
-                    self.fast_placings[fast[i]].append(i+1)
+                    self.fast_placings[fast[i]].append(i + 1)
                 for i in range(len(short)):
-                    self.short_placings[short[i]].append(i+1)
+                    self.short_placings[short[i]].append(i + 1)
         except cses.CSESError:
-            pass # TODO log here?
+            pass  # TODO log here?
 
     def leaderboard(self, placings, num):
-        leaderboard = sorted(
-            ((k,score(v)) for k,v in placings.items()),
-            key=lambda x: x[1], reverse=True)
+        leaderboard = sorted(((k, score(v)) for k, v in placings.items()), key=lambda x: x[1], reverse=True)
 
         if not leaderboard:
             return 'Failed to load :<'
 
         top = leaderboard[:num]
-        maxname = max(len(name) for name,_ in top)
+        maxname = max(len(name) for name, _ in top)
 
         S = []
-        for user,points in top:
-            userpad=user.rjust(maxname)
-            hist = [placings[user].count(i+1) for i in range(5)]
+        for user, points in top:
+            userpad = user.rjust(maxname)
+            hist = [placings[user].count(i + 1) for i in range(5)]
             ranks = ' '.join(f'{count:3d}' for count in hist)
             S.append(f'{userpad} | {ranks} | {points:3d} points')
 
-        header=(maxname+3)*' ' + '1st 2nd 3rd 4th 5th'
+        header = (maxname + 3) * ' ' + '1st 2nd 3rd 4th 5th'
         return '\n'.join([header] + S)
 
     @property
@@ -71,16 +68,11 @@ class CSES(commands.Cog):
     def shortest(self, num=10):
         return self.leaderboard(self.short_placings, num)
 
-
     @commands.command(brief='Shows compiled CSES leaderboard')
     async def cses(self, ctx):
         """Shows compiled CSES leaderboard."""
-        await ctx.send(
-                '```\n'
-                'Fastest\n'+self.fastest+'\n\n'+
-                'Shortest\n'+self.shortest+'\n'+
-                '```'
-                )
+        await ctx.send('```\n' 'Fastest\n' + self.fastest + '\n\n' + 'Shortest\n' + self.shortest + '\n' + '```')
+
 
 def setup(bot):
     bot.add_cog(CSES(bot))
