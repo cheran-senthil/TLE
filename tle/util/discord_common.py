110fab3174a2eca2f7232dfe8d8292a4eee228b3
meooow25 <soumiksarkar.3120@gmail.com>
2019-04-12 03:26:44 +0530

    Switch future_contests.py to common embed messages.

--- tle/util/discord_common.py
+++ tle/util/discord_common.py
@@ -3,20 +3,20 @@ import random
 import discord
 
 _CF_COLORS = (0xFFCA1F, 0x198BCC, 0xFF2020)
-_SUCCESS_GREEN = 0x00A000
+_SUCCESS_GREEN = 0x28A745
 _ALERT_AMBER = 0xFFBF00
 
 
-def simple_embed(desc, color=discord.Embed.Empty):
+def embed_neutral(desc, color=discord.Embed.Empty):
     return discord.Embed(description=desc, color=color)
 
 
 def embed_success(desc):
-    return simple_embed(desc, _SUCCESS_GREEN)
+    return discord.Embed(description=desc, color=_SUCCESS_GREEN)
 
 
 def embed_alert(desc):
-    return simple_embed(desc, _ALERT_AMBER)
+    return discord.Embed(description=desc, color=_ALERT_AMBER)
 
 
 def cf_color_embed(**kwargs):
