25d2370030a478ad85df0e549b4112e56a4dba97
Kabeer <kabeer.205@gmail.com>
2019-08-24 10:20:44 +0530

    Adding logging to file

--- tle/__main__.py
+++ tle/__main__.py
@@ -14,9 +14,11 @@ from tle.util import discord_common
 
 
 def setup():
-    # logging
-    logging.basicConfig(format='{asctime}:{levelname}:{name}:{message}', style='{',
+    # logging to file
+    logging.basicConfig(filename="log.txt",filemode="a",format='{asctime}:{levelname}:{name}:{message}', style='{',
                         datefmt='%d-%m-%Y %H:%M:%S', level=logging.INFO)
+    # logging to console
+    logging.getLogger().addHandler(logging.StreamHandler())
 
     # matplotlib and seaborn
     plt.rcParams['figure.figsize'] = 7.0, 3.5
