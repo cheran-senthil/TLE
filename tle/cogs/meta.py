b09c8062f15fa33c6c8684bafdc58775f0163f00
Alberto <albertopoljak@gmail.com>
2019-10-03 15:38:18 +0200

    Ping command update - removed useless whitespace in output message

--- tle/cogs/meta.py
+++ tle/cogs/meta.py
@@ -74,7 +74,7 @@ class Meta(commands.Cog):
         message = await ctx.send(':ping_pong: Pong!')
         end = time.perf_counter()
         duration = (end - start) * 1000
-        await message.edit(content=f'REST API latency: {int(duration)}ms \n'
+        await message.edit(content=f'REST API latency: {int(duration)}ms\n'
                                    f'Gateway API latency: {int(self.bot.latency * 1000)}ms')
 
     @meta.command(brief='Get git information')
