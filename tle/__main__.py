432642e0b314401675b24b76e8a78a3741237d9a
Kabeer <kabeer.205@gmail.com>
2019-10-10 14:14:39 +0530

    Added logging to file on daily interval

--- tle/__main__.py
+++ tle/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+from logging.handlers import TimedRotatingFileHandler
 import os
 from os import environ
 from pathlib import Path
@@ -14,9 +15,13 @@ from tle.util import discord_common
 
 
 def setup():
-    # logging
+    # logging to console and File on Daily interval
     logging.basicConfig(format='{asctime}:{levelname}:{name}:{message}', style='{',
-                        datefmt='%d-%m-%Y %H:%M:%S', level=logging.INFO)
+                        datefmt='%d-%m-%Y %H:%M:%S', level=logging.INFO,
+                        handlers=[logging.StreamHandler(),
+                                  TimedRotatingFileHandler("TleBot.log",when="D",interval=1,backupCount=0,utc=True)
+                                  ]
+                        )
 
     # matplotlib and seaborn
     plt.rcParams['figure.figsize'] = 7.0, 3.5
