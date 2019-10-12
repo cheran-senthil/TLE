cde767a909e27588659f537469c390e2d5b605ef
Anton Älgmyr <algmyr@google.com>
2019-10-12 23:30:10 +0200

    Make suggested fixes

--- tle/util/table.py
+++ tle/util/table.py
@@ -1,15 +1,10 @@
 import unicodedata
 
-full_width = 1.66667
-width_mapping = {'F': full_width, 'H': 1, 'W': full_width, 'Na': 1, 'N': 1, 'A': 1}
+FULL_WIDTH = 1.66667
+WIDTH_MAPPING = {'F': FULL_WIDTH, 'H': 1, 'W': FULL_WIDTH, 'Na': 1, 'N': 1, 'A': 1}
 
 def width(s):
-    result = 0
-    for c in s:
-        w = unicodedata.east_asian_width(c)
-        result += width_mapping[w]
-    return round(result)
-
+    return round(sum(WIDTH_MAPPING[unicodedata.east_asian_width(c)] for c in s))
 
 
 class Content:
