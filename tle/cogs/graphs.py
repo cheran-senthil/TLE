1790922f78d4eb33b83a7f44f6d356abdef3511f
Mislav Blažević <mislav.blazevic5@gmail.com>
2019-12-15 20:00:01 +0100

    misc

--- tle/cogs/graphs.py
+++ tle/cogs/graphs.py
@@ -334,7 +334,7 @@ class Graphs(commands.Cog):
     async def solved(self, ctx, *args: str):
         """Shows a histogram of problems solved on Codeforces for the handles provided.
         e.g. ;plot solved meooow +contest +virtual +outof +dp"""
-        team, types, tags, dlo, dhi, rlo, dri, args = cf_common.filter_sub_args(args)
+        team, types, tags, dlo, dhi, rlo, rhi, args = cf_common.filter_sub_args(args)
         handles = args or ('!' + str(ctx.author),)
         handles = await cf_common.resolve_handles(ctx, self.converter, handles)
         resp = [await cf.user.status(handle=handle) for handle in handles]
@@ -403,7 +403,7 @@ class Graphs(commands.Cog):
                   usage='[handles] [+practice] [+contest] [+virtual] [+outof] [+team] [+tag..] [r>rating] [r<rating] [>ddmmyyyy] [<ddmmyyyy]')
     async def hist(self, ctx, *args: str):
         """Shows the actual histogram of problems solved on Codeforces for the handles provided."""
-        team, types, tags, dlo, dhi, rlo, dri, args = cf_common.filter_sub_args(args)
+        team, types, tags, dlo, dhi, rlo, rhi, args = cf_common.filter_sub_args(args)
         handle = args[0] if handles else '!' + str(ctx.author)
         handle, = await cf_common.resolve_handles(ctx, self.converter, (handle,))
         subs = await cf.user.status(handle=handle)
