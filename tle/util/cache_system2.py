3cefc32a1a1316a6ead88373ea4d1642b3789ac0
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-05-04 08:25:12 +0200

    Added generate vc ranklist

--- tle/util/cache_system2.py
+++ tle/util/cache_system2.py
@@ -605,6 +605,23 @@ class RanklistCache:
 
         return ranklist
 
+    async def generate_vc_ranklist(self, contest_id, handles):
+        contest, problems, standings = await cf.contest.standings(contest_id=contest_id,
+                                                                  show_unofficial=True)
+        now = time.time()
+        standings = [row for row in standings
+                    if row.party.participantType == 'VIRTUAL'
+                    and row.party.teamId is None
+                    and ((row.party.members[0].handle in handles) or handles is None)
+                    ]
+
+        handles = [row.party.members[0].handle for row in standings]
+        current_rating = await CacheSystem.getUsersVCRating(handles)
+        ranklist = Ranklist(contest, problems, standings, now, is_rated=True)
+        ranklist.predict(current_rating)
+
+        return ranklist
+
     async def _fetch(self, contests):
         ranklist_by_contest = {}
         for contest in contests:
@@ -643,3 +660,10 @@ class CacheSystem:
         users_effective_rating_dict = {user.handle: user.effective_rating
                                   for user in ratedList}
         return users_effective_rating_dict
+
+    @staticmethod
+    @cached(ttl = 30 * 60)
+    async def getUsersVCRating(user_ids):
+        users_vc_rating_dict = {user_id: cf_common.user_db.get_vc_rating(user_id, True)
+                                for user_id in user_ids}
+        return users_vc_rating_dict
