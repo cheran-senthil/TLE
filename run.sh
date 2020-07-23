e0b37c2e9e8e89e1fdacb7e250df836081664126
abdulrahmannaser <abdonaser_97@outlook.com>
2020-07-23 23:38:28 +0200

    [ADD] remote branch to original repo

--- run.sh
+++ run.sh
@@ -6,7 +6,25 @@ cd "$(dirname "$0")"
 [ -e environment ] && . ./environment
 
 while true; do
-    git pull
+    
+    # Contributers should pull from the original repo & push to the forked one
+    # if the cloned repo is forked & upstream branch doesn't exist -> add upstream branch to Cheran's repo to pull updates
+    # else if the remote branch already exists -> pull updates from the current branch
+    # else pull from the cloned repo
+    
+    if ! [[ $(git remote -v | grep -E 'origin.*https://github.com/cheran-senthil/TLE.git') ]]; then # cloned repo is forked
+	    if ! [[ $(git remote show | grep upstream) ]]; then # upstream remote branch doesn't exixt
+		    echo "Adding upstream branch to author's repo"
+		    git remote add upstream https://github.com/cheran-senthil/TLE.git 
+	    fi
+	    if [[ $(git remote -v | grep -E 'upstream.*https://github.com/cheran-senthil/TLE.git') ]]; then # upstream remote branch exists
+		    echo "Pulling updates from author's repo"
+		    git pull upstream $(git symbolic-ref --short HEAD)
+	    fi
+    else # pull updates from cloned repo remote branches
+	    echo "Pulling updates"
+	    git pull
+    fi
     poetry install
     FONTCONFIG_FILE=$PWD/extra/fonts.conf poetry run python -m tle
 
