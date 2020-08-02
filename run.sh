47151a50693f865fb7138870403e34dae1af5d53
Aryan Choudhary <github@aryanc403.com>
2020-08-02 11:10:52 +0530

    Revert run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ cd "$(dirname "$0")"
 [ -e environment ] && . ./environment
 
 while true; do
-#    git pull
-#    poetry install
+    git pull
+    poetry install
     FONTCONFIG_FILE=$PWD/extra/fonts.conf poetry run python -m tle
 
     (( $? != 42 )) && break
