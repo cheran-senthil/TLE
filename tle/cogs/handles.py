ffc1ce847914ff828394f8f520b048eb8603a5de
Denjell <denjellboone@gmail.com>
2021-05-31 23:04:43 +0200

    Gitgud change: Reduce contest limit to 1 for people appear on division leaderboard for monthlygitgud

--- tle/cogs/handles.py
+++ tle/cogs/handles.py
@@ -517,40 +517,6 @@ class Handles(commands.Cog):
             lines += failed
         return discord_common.embed_success('\n'.join(lines))
 
-    @commands.command(brief="Show gudgitters of the last 30 days", aliases=["recentgitgudders"])
-    async def recentgudgitters(self, ctx):
-        """Show the list of users of gitgud with their scores."""
-        minimal_finish_time = int(datetime.datetime.now().timestamp())-30*24*60*60
-        results = cf_common.user_db.get_gudgitters_last(minimal_finish_time)
-        res = {}
-        for entry in results:
-            res[entry[0]] = 0
-        for entry in results:
-            res[entry[0]] += _GITGUD_SCORE_DISTRIB[(int(entry[1])+300)//100]
-        
-        rankings = []
-        index = 0
-        for user_id, score in sorted(res.items(), key=lambda item: item[1], reverse=True):
-            member = ctx.guild.get_member(int(user_id))
-            if member is None:
-                continue
-            if score > 0:
-                handle = cf_common.user_db.get_handle(user_id, ctx.guild.id)
-                user = cf_common.user_db.fetch_cf_user(handle)
-                if user is None:
-                    continue
-                discord_handle = member.display_name
-                rating = user.rating
-                rankings.append((index, discord_handle, handle, rating, score))
-                index += 1
-            if index == 20:
-                break
-
-        if not rankings:
-            raise HandleCogError('No one has completed a gitgud challenge, send ;gitgud to request and ;gotgud to mark it as complete')
-        discord_file = get_gudgitters_image(rankings)
-        await ctx.send(file=discord_file)
-
     @commands.command(brief="Show gudgitters", aliases=["gitgudders"])
     async def gudgitters(self, ctx):
         """Show the list of users of gitgud with their scores."""
@@ -644,7 +610,7 @@ class Handles(commands.Cog):
                 rating_changes = [change for change in rating_changes if change.ratingUpdateTimeSeconds < start_time]
                 rating_changes.sort(key=lambda a: a.ratingUpdateTimeSeconds)
                 if division is not None:
-                    if len(rating_changes) < 6: 
+                    if len(rating_changes) < 1: 
                         continue
                     if rating_changes[-1] is None: continue
                     if rating_changes[-1].newRating < _DIVISION_RATING_LOW[division-1] or rating_changes[-1].newRating > _DIVISION_RATING_HIGH[division-1]:
