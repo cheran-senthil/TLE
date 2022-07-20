a8d841ad010a71af40e051353e6aa7201c2f3960
Denjell <denjellboone@gmail.com>
2022-07-20 23:08:55 +0200

    Logic for finding combined rounds

--- tle/cogs/contests.py
+++ tle/cogs/contests.py
@@ -815,7 +815,7 @@ class Contests(commands.Cog):
         """Estimation of contest problem ratings
         """
         contests = await cf.contest.list()
-        reqcontest = [contest for contest in contests if int(contest.id) == int(contest_id)]
+        reqcontest = [contest for contest in contests if contest.id == contest_id]
         combined = [contest for contest in contests if reqcontest[0].startTimeSeconds == contest.startTimeSeconds]
         raise ContestCogError(f'Size: {len(combined)}')
 
