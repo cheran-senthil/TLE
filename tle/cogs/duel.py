a70985488be470a8cacbc7f9ba07c3471c972be2
Denjell <73618585+Denjell@users.noreply.github.com>
2022-10-20 00:13:46 +0200

    Added log messages to cyclic task

--- tle/cogs/duel.py
+++ tle/cogs/duel.py
@@ -131,7 +131,7 @@ class Dueling(commands.Cog):
         logger.info(f'duel._check_ongoing_duels_for_guild running for {guild_id}')
         # check for ongoing duels that are older than _DUEL_MAX_DUEL_DURATION
         data = cf_common.user_db.get_ongoing_duels(guild_id)
-        guild = self.bot.fetch_guild(guild_id)
+        guild = await self.bot.fetch_guild(guild_id)
 
         for entry in data:
             start_time, name, challenger_id, challengee_id, duelid, dtype = entry
