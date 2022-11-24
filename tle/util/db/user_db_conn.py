40548def9004a23e5d27010d9071f8ef20871e35
Denjell <73618585+Denjell@users.noreply.github.com>
2022-11-24 13:18:03 +0100

    Lockout integration (#14)
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Challenge command finished
    
    * Added problems and _invalidate
    
    * Bugfix: _invalidate / implementation of problems_embed
    
    * Bugfix: _invalidate / implementation of problems_embed
    
    * Bugfix: _invalidate / implementation of problems_embed
    
    * Bugfix: _invalidate / implementation of problems_embed
    
    * Bugfix: _invalidate / implementation of problems_embed
    
    * Bugfix: _invalidate / implementation of problems_embed
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added and refactored update method
    
    * Added ongoing command
    
    * Added ongoing command
    
    * Added recent command
    
    * Added recent command

--- tle/util/db/user_db_conn.py
+++ tle/util/db/user_db_conn.py
@@ -46,7 +46,7 @@ class DuelType(IntEnum):
     OFFICIAL = 1
     ADJUNOFFICIAL = 2
     ADJOFFICIAL = 3
-    
+
 class RatedVC(IntEnum):
     ONGOING = 0
     FINISHED = 1
@@ -141,7 +141,7 @@ class UserDbConn:
                 guild_id TEXT PRIMARY KEY,
                 channel_id TEXT
             )
-        ''')        
+        ''')
         self.conn.execute('''
             CREATE TABLE IF NOT EXISTS "challenge" (
                 "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -266,6 +266,45 @@ class UserDbConn:
             )
         ''')
 
+        self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS round_settings (
+                guild_id TEXT PRIMARY KEY,
+                channel_id TEXT
+            )
+        ''')
+        self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS lockout_ongoing_rounds (
+                "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+                "guild" TEXT,
+                "users" TEXT,
+                "rating" TEXT,
+                "points" TEXT,
+                "time" INT,
+                "problems" TEXT,
+                "status" TEXT,
+                "duration" INTEGER,
+                "repeat" INTEGER,
+                "times" TEXT
+            )
+        ''')
+
+        self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS lockout_finished_rounds(
+                "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+                "guild" TEXT,
+                "users" TEXT,
+                "rating" TEXT,
+                "points" TEXT,
+                "time" INT,
+                "problems" TEXT,
+                "status" TEXT,
+                "duration" INTEGER,
+                "repeat" INTEGER,
+                "times" TEXT,
+                "end_time" INT
+            )
+            ''')
+
     # Helper functions.
 
     def _insert_one(self, table: str, columns, values: tuple):
@@ -348,12 +387,12 @@ class UserDbConn:
             SELECT user_id, rating_delta FROM challenge WHERE finish_time >= ? ORDER BY user_id
         '''
         return self.conn.execute(query, (timestamp,)).fetchall()
-        
+
     def get_gudgitters_timerange(self, timestampStart, timestampEnd):
         query = '''
             SELECT user_id, rating_delta FROM challenge WHERE finish_time >= ? AND finish_time <= ? ORDER BY user_id
         '''
-        return self.conn.execute(query, (timestampStart,timestampEnd)).fetchall()        
+        return self.conn.execute(query, (timestampStart,timestampEnd)).fetchall()
 
     def get_gudgitters(self):
         query = '''
@@ -702,7 +741,7 @@ class UserDbConn:
     def get_duel_wins(self, userid, guild_id):
         query = f'''
             SELECT start_time, finish_time, problem_name, challenger, challengee FROM duel
-            WHERE ((challenger = ? AND winner == {Winner.CHALLENGER}) OR (challengee = ? AND winner == {Winner.CHALLENGEE})) AND status = {Duel.COMPLETE} AND guild_id = ? 
+            WHERE ((challenger = ? AND winner == {Winner.CHALLENGER}) OR (challengee = ? AND winner == {Winner.CHALLENGEE})) AND status = {Duel.COMPLETE} AND guild_id = ?
         '''
         return self.conn.execute(query, (userid, userid, guild_id)).fetchall()
 
@@ -1060,8 +1099,8 @@ class UserDbConn:
             INSERT INTO training_problems (training_id, issue_time, problem_name, contest_id, p_index, rating, status)
             VALUES (?, ?, ?, ?, ?, ?, {TrainingProblemStatus.ACTIVE})
         '''
-        
-        cur = self.conn.cursor()            
+
+        cur = self.conn.cursor()
         cur.execute(query1, (training_id, issue_time, prob.name, prob.contestId, prob.index, prob.rating))
         if cur.rowcount != 1:
             self.conn.rollback()
@@ -1083,9 +1122,9 @@ class UserDbConn:
 
     def get_training_skips(self, user_id):
         query = f'''
-            SELECT tp.problem_name 
+            SELECT tp.problem_name
             FROM training_problems tp, trainings tr
-            WHERE tp.training_id = tr.id 
+            WHERE tp.training_id = tr.id
             AND (tp.status = {TrainingProblemStatus.SKIPPED} OR tp.status = {TrainingProblemStatus.INVALIDATED})
             AND tr.user_id = ?
         '''
@@ -1094,35 +1133,35 @@ class UserDbConn:
 
     def train_get_num_solves(self, training_id):
         query = f'''
-            SELECT COUNT(*) FROM training_problems 
+            SELECT COUNT(*) FROM training_problems
             WHERE training_id = ? AND status == {TrainingProblemStatus.SOLVED}
         '''
         return self.conn.execute(query, (training_id,)).fetchone()[0]
 
     def train_get_num_skips(self, training_id):
         query = f'''
-            SELECT COUNT(*) FROM training_problems 
+            SELECT COUNT(*) FROM training_problems
             WHERE training_id = ? AND status == {TrainingProblemStatus.SKIPPED}
         '''
         return self.conn.execute(query, (training_id,)).fetchone()[0]
 
     def train_get_num_slow_solves(self, training_id):
         query = f'''
-            SELECT COUNT(*) FROM training_problems 
+            SELECT COUNT(*) FROM training_problems
             WHERE training_id = ? AND status == {TrainingProblemStatus.SOLVED_TOO_SLOW}
         '''
         return self.conn.execute(query, (training_id,)).fetchone()[0]
 
     def train_get_start_rating(self, training_id):
         query = f'''
-            SELECT rating FROM training_problems 
+            SELECT rating FROM training_problems
             WHERE training_id = ?
         '''
         return self.conn.execute(query, (training_id,)).fetchone()[0]
 
     def train_get_max_rating(self, training_id):
         query = f'''
-            SELECT MAX(rating) FROM training_problems 
+            SELECT MAX(rating) FROM training_problems
             WHERE training_id = ? AND status == {TrainingProblemStatus.SOLVED}
         '''
         return self.conn.execute(query, (training_id,)).fetchone()[0]
@@ -1131,13 +1170,139 @@ class UserDbConn:
         query = f'''
             SELECT tr.user_id, tp.rating, min(tp.finish_time-tp.issue_time)
             FROM training_problems tp, trainings tr
-            WHERE tp.training_id = tr.id 
+            WHERE tp.training_id = tr.id
             AND (tp.status = {TrainingProblemStatus.SOLVED} OR tp.status = {TrainingProblemStatus.SOLVED_TOO_SLOW})
             GROUP BY tp.rating
         '''
         return self.conn.execute(query).fetchall()
 
+    ### Lockout round
+
+
+    def set_round_channel(self, guild_id, channel_id):
+        query = ('INSERT OR REPLACE INTO round_settings '
+                 ' (guild_id, channel_id) VALUES (?, ?)'
+                 )
+        with self.conn:
+            self.conn.execute(query, (guild_id, channel_id))
+
+    def get_round_channel(self, guild_id):
+        query = ('SELECT channel_id '
+                 'FROM round_settings '
+                 'WHERE guild_id = ?')
+        channel_id = self.conn.execute(query, (guild_id,)).fetchone()
+        return int(channel_id[0]) if channel_id else None
+
+    def create_ongoing_round(self, guild_id, timestamp, users, rating, points, problems, duration, repeat):
+        query = f'''
+            INSERT INTO lockout_ongoing_rounds (guild, users, rating, points, time, problems, status, duration, repeat, times)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        '''
+        cur = self.conn.cursor()
+        cur.execute(query, (guild_id, ' '.join([f"{x.id}" for x in users]), 
+                                      ' '.join(map(str, rating)),
+                                      ' '.join(map(str, points)), 
+                                      timestamp, 
+                                      ' '.join([f"{x.contestId}/{x.index}" for x in problems]), 
+                                      ' '.join('0' for i in range(len(users))),
+                                      duration, 
+                                      repeat, 
+                                      ' '.join(['0'] * len(users)))
+                    )
+        self.conn.commit()
+        cur.close()
+
+    def create_finished_round(self, round_info, timestamp):
+        query = f'''
+                    INSERT INTO lockout_finished_rounds (guild, users, rating, points, time, problems, status, duration, repeat, times, end_time)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                '''
+        cur = self.conn.cursor()
+        cur.execute(query, (round_info.guild, round_info.users, round_info.rating, round_info.points, round_info.time,
+                                round_info.problems, round_info.status, round_info.duration, round_info.repeat,
+                                round_info.times, timestamp))
+        self.conn.commit()
+        cur.close()                
+
+    def update_round_status(self, guild, user, status, problems, timestamp):
+        query = f"""
+                    UPDATE lockout_ongoing_rounds 
+                    SET
+                    status = ?, 
+                    problems = ?,
+                    times = ?
+                    WHERE
+                    guild = ? AND users LIKE ? 
+                """
+        cur = self.conn.cursor()
+        cur.execute(query,
+                     (' '.join([str(x) for x in status]), ' '.join(problems), ' '.join([str(x) for x in timestamp]),
+                      guild, f"%{user}%"))
+        self.conn.commit()
+        cur.close()
+
+    def get_round_info(self, guild_id, users):
+        query = f'''
+                    SELECT * FROM lockout_ongoing_rounds
+                    WHERE
+                    guild = ? AND users LIKE ?
+                 '''
+        cur = self.conn.cursor()
+        cur.execute(query, (guild_id, f"%{users}%"))
+        data = cur.fetchone()
+        cur.close()
+        Round = namedtuple('Round', 'guild users rating points time problems status duration repeat times')
+        return Round(data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10])
+
+    def check_if_user_in_ongoing_round(self, guild, user):
+        query = f'''
+                    SELECT * FROM lockout_ongoing_rounds
+                    WHERE
+                    users LIKE ? AND guild = ?
+                '''
+        cur = self.conn.cursor()
+        cur.execute(query, (f"%{user}%", guild))
+        data = cur.fetchall()
+        cur.close()
+        if len(data) > 0:
+            return True
+        return False
+
+    def delete_round(self, guild, user):
+        query = f'''
+                    DELETE FROM lockout_ongoing_rounds
+                    WHERE
+                    guild = ? AND users LIKE ?
+                '''
+        cur = self.conn.cursor()
+        cur.execute(query, (guild, f"%{user}%"))
+        self.conn.commit()
+        cur.close()    
+
+    def get_ongoing_rounds(self, guild):
+        query = f'''
+                    SELECT * FROM lockout_ongoing_rounds WHERE guild = ?
+                '''
+        cur = self.conn.cursor()
+        cur.execute(query, (guild,))
+        res = cur.fetchall()
+        cur.close()
+        Round = namedtuple('Round', 'guild users rating points time problems status duration repeat times')
+        return [Round(data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]) for data in res]
+
+    def get_recent_rounds(self, guild, user=None):
+        query = f'''
+                    SELECT * FROM lockout_finished_rounds 
+                    WHERE guild = ? AND users LIKE ?
+                    ORDER BY end_time DESC
+                '''
+        cur = self.conn.cursor()
+        cur.execute(query, (guild, '%' if user is None else f'%{user}%'))
+        res = cur.fetchall()
+        cur.close()
+        Round = namedtuple('Round', 'guild users rating points time problems status duration repeat times end_time')
+        return [Round(data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11]) for data in res]
+
     def close(self):
         self.conn.close()
-
 
