e8c714d44ba2e75ca499d34482fbf92f9d85b316
Null Chilly <nullchilly@gmail.com>
2023-12-17 15:44:56 +0700

    Fix starboard images not embedding due to attachment URL change

--- tle/cogs/starboard.py
+++ tle/cogs/starboard.py
@@ -66,7 +66,7 @@ class Starboard(commands.Cog):
 
         if message.attachments:
             file = message.attachments[0]
-            if file.url.lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
+            if file.filename.lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
                 embed.set_image(url=file.url)
             else:
                 embed.add_field(name='Attachment', value=f'[{file.filename}]({file.url})', inline=False)
