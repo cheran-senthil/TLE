7f4d22196b57c487e56520b4bc58d2973b55926e
Anton Älgmyr <anton@algmyr.se>
2025-07-10 06:50:46 +0200

    Fix bug introduced by manual reformatting

--- tle/util/db/user_db_conn.py
+++ tle/util/db/user_db_conn.py
@@ -588,7 +588,7 @@ class UserDbConn:
         cfg = self.conn.execute(
             """
             SELECT channel_id
-            FROM starboard_config_v1 WHERE guild_id=? AND emoji=?,
+            FROM starboard_config_v1 WHERE guild_id=? AND emoji=?
             """,
             (guild_id, emoji),
         ).fetchone()
@@ -597,7 +597,7 @@ class UserDbConn:
         emo = self.conn.execute(
             """
             SELECT threshold, color
-            FROM starboard_emoji_v1 WHERE guild_id=? AND emoji=?,
+            FROM starboard_emoji_v1 WHERE guild_id=? AND emoji=?
             """,
             (guild_id, emoji),
         ).fetchone()
