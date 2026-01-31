c431c0452131e1e0308ed5b7313a0275c3458c62
Anton Älgmyr <anton@algmyr.se>
2026-01-31 16:04:54 +0100

    Fix font downloading (based on #542)
    Now targets github hosted files which should be more stable.
    
    Co-authored-by: Soad Mahmud <108691772+soadmahmud@users.noreply.github.com>

--- tle/util/font_downloader.py
+++ tle/util/font_downloader.py
@@ -1,34 +1,30 @@
 import logging
 import os
 import urllib.request
-from io import BytesIO
-from zipfile import ZipFile
 
 from tle import constants
 
-URL_BASE = 'https://noto-website-2.storage.googleapis.com/pkgs/'
-FONTS = [
+URL_BASE = 'https://github.com/notofonts/noto-cjk/raw/main/Sans/OTC/'
+FONTS = {
     constants.NOTO_SANS_CJK_BOLD_FONT_PATH,
     constants.NOTO_SANS_CJK_REGULAR_FONT_PATH,
-]
+}
 
 logger = logging.getLogger(__name__)
-
-
-def _unzip(font, archive):
-    with ZipFile(archive) as zipfile:
-        if font not in zipfile.namelist():
-            raise KeyError(
-                f'Expected font file {font} not present in downloaded zip archive.'
-            )
-        zipfile.extract(font, constants.FONTS_DIR)
 
 
 def _download(font_path):
     font = os.path.basename(font_path)
     logger.info(f'Downloading font `{font}`.')
-    with urllib.request.urlopen(f'{URL_BASE}{font}.zip') as resp:
-        _unzip(font, BytesIO(resp.read()))
+    with urllib.request.urlopen(f'{URL_BASE}{font}') as resp:
+        if resp.status != 200:
+            msg = f'Failed to download font `{font}`. HTTP status code: {resp.status}.'
+            logger.error(msg)
+            raise ConnectionError(msg)
+        logger.info(f'Successfully downloaded font `{font}`.')
+        data = resp.read()
+        with open(font_path, 'wb') as f:
+            f.write(data)
 
 
 def maybe_download():
